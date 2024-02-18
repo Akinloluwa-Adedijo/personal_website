4fd619d9cd09fbb96efbe6b4ef5322a01b2d4bbb
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2024-02-17 18:31:33 -0800

    added home text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import {
   Box,
+  Button,
   Center,
   Stack,
   Heading,
+  Text,
   useColorMode,
   useColorModeValue,
 } from "@chakra-ui/react";
+import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 const Home = () => {
   return (
@@ -23,9 +26,34 @@ const Home = () => {
         <Heading
           fontWeight={600}
           fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
-          color={useColorModeValue("tect.light")}
-        ></Heading>
+          color={useColorModeValue("text.light", "text.dark")}
+        >
+          Hey,
+          <Heading
+            fontWeight={600}
+            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
+            color={useColorModeValue("accent.light", "accent.dark")}
+          >
+            I'm Akinloluwa Adedijo
+          </Heading>
+          <br />
+          <Text
+            fontWeight={600}
+            fontSize={40}
+            as={"span"}
+            color={useColorModeValue("text.light", "text,dark")}
+          >
+            Welcome to my website
+          </Text>
+        </Heading>
       </Stack>
+      <Button
+        bg={useColorModeValue("primary", "primary")}
+        _hover={"none"}
+        color={useColorModeValue("text.light", "text.light")}
+      >
+        About Me
+      </Button>
     </Box>
   );
 };
