78d87664e74b41dbac85b6149cd8f1d36e46f992
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2024-02-23 19:12:05 -0800

    finished intro section of home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,50 +4,54 @@ import {
   Button,
   Highlight,
   Stack,
+  VStack,
+  HStack,
   Heading,
   Center,
+  Spacer,
+  Flex,
+  Avatar,
   Text,
+  Image,
   useColorModeValue,
 } from "@chakra-ui/react";
-import { ArrowForwardIcon } from "@chakra-ui/icons";
+import Typewriter from "typewriter-effect";
 
 const Home = () => {
   return (
-    <Box bg={useColorModeValue("background.light", "background.dark")} mt={20}>
-      <Stack
-        as={"Box"}
-        textAlign={"center"}
-        spacing={{ base: "8", md: "14" }}
-        py={{ base: "20", md: "16" }}
-      >
-        <Heading
-          fontWeight={600}
-          lineHeight={"tall"}
-          fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
-          color={useColorModeValue("text.light", "text.dark")}
-        >
-          <Highlight
-            query={"Akinloluwa Adedijo"}
-            styles={{ px: "2", py: "1", rounded: "full", bg: "primary" }}
-          >
-            Hey, I'm Akinloluwa Adedijo
-          </Highlight>
-          <Text
-            fontWeight={300}
-            fontSize={{ base: "xl", sm: "2xl", md: "4xl" }}
-          >
-            Welcome to my personal website
-          </Text>
-          <Button
-            bg={"primary"}
-            rounded={"full"}
-            rightIcon={<ArrowForwardIcon />}
-            size={{ base: "sm", sm: "md", md: "lg" }}
-          >
-            About Me
-          </Button>
-        </Heading>
-      </Stack>
+    <Box
+      bg={useColorModeValue("background.light", "background.dark")}
+      mt={5}
+      h={"80vh"}
+    >
+      <Center>
+        <Flex alignItems={"center"} minW={"200px"} p={10} gap={10}>
+          <Box textAlign={"left"}>
+            <Heading
+              as="h2"
+              color={useColorModeValue("accent.light", "accent.dark")}
+            >
+              Akinloluwa Adedijo
+            </Heading>
+            <Typewriter
+              options={{
+                strings: [
+                  "Aspiring Software Developer",
+                  "Aspiring GIS Developer",
+                  "Graphic Designer",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </Box>
+          <Avatar
+            name="Akinloluwa Adedijo"
+            bg={useColorModeValue("accent.light", "accent.dark")}
+            size="lg"
+          />
+        </Flex>
+      </Center>
     </Box>
   );
 };
