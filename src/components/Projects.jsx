79720dcb93110f505924127d95c02e767b1ed403
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2024-02-27 21:49:12 -0800

    added button color on hover & active

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,6 +10,32 @@ import {
 } from "@chakra-ui/react";
 
 const names = ["All", "Web Development", "GIS"];
+
+const ProjectToggleButton = (props) => {
+  const { children } = props;
+  // console.log(children);
+  return (
+    <Box
+      as="button"
+      px={2}
+      py={1}
+      height="36px"
+      rounded="md"
+      color={useColorModeValue("text.light", "primary")}
+      fontSize="lg"
+      _hover={{
+        bg: "primary",
+        color: "text.light",
+      }}
+      _active={{
+        bg: "primary",
+        color: "text.light",
+      }}
+    >
+      {children}
+    </Box>
+  );
+};
 const Projects = () => {
   return (
     <Box h={"max-content"}>
@@ -24,26 +50,8 @@ const Projects = () => {
         <Flex>
           <Flex direction={"row"} gap={10}>
             {names.map((name) => (
-              <Button
-                color={useColorModeValue("text.light", "text.dark")}
-                colorScheme="primary"
-                variant={"outline"}
-                key={name}
-              >
-                {name}
-              </Button>
+              <ProjectToggleButton key={name}>{name}</ProjectToggleButton>
             ))}
-            {/* <Button
-              color={useColorModeValue("text.light", "text.dark")}
-              colorScheme="primary"
-              variant={"outline"}
-            >
-              All
-            </Button>
-            <Spacer />
-            <Button>Web Development</Button>
-            <Spacer />
-            <Button>GIS</Button> */}
           </Flex>
         </Flex>
       </Center>
