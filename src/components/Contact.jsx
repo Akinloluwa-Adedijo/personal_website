b3d1bf13c006d5247b3f614c26ab26633ec9d32b
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2024-03-19 21:21:49 -0700

    added contact icons and links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,85 @@
 import React from "react";
-import { Box, Heading, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+  Spacer,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { EmailIcon } from "@chakra-ui/icons";
-import { FaGithub, FaLinkedin, FaBehance, FaInstagram } from "react-icons/fa";
+import {
+  FaLinkedin,
+  FaBehanceSquare,
+  FaBehance,
+  FaInstagramSquare,
+} from "react-icons/fa";
+import { IoLogoGithub, IoIosMail } from "react-icons/io";
 
 const Contact = () => {
   return (
-    <Box position="bottom">
-      <Heading textColor={useColorModeValue("primary.light", "primary.dark")}>
-        Get In Touch
-      </Heading>
-      <Box></Box>
-    </Box>
+    <Center>
+      <Box position="bottom">
+        <Heading textColor={useColorModeValue("primary.light", "primary.dark")}>
+          Get In Touch
+        </Heading>
+        <Flex w={{ base: "xs", md: "md" }}>
+          <Box as="a" href="mailto:adedijoakinloluwa@gmail.com">
+            <Icon
+              as={IoIosMail}
+              boxSize={{ base: 50, md: 75 }}
+              color={useColorModeValue("primary.light", "primary.dark")}
+              _hover={{
+                color: useColorModeValue("accent.light", "accent.dark"),
+                cursor: "pointer",
+              }}
+            />
+          </Box>
+          <Spacer />
+          <Box as="a" href="https://github.com/Akinloluwa-Adedijo">
+            <Icon
+              as={IoLogoGithub}
+              boxSize={{ base: 50, md: 75 }}
+              color={useColorModeValue("primary.light", "primary.dark")}
+              _hover={{
+                color: useColorModeValue("accent.light", "accent.dark"),
+                cursor: "pointer",
+              }}
+            />
+          </Box>
+          <Spacer />
+          <Box
+            as="a"
+            href="https://www.linkedin.com/in/akinloluwa-adedijo-a362b2177/"
+          >
+            <Icon
+              as={FaLinkedin}
+              boxSize={{ base: 50, md: 75 }}
+              color={useColorModeValue("primary.light", "primary.dark")}
+              _hover={{
+                color: useColorModeValue("accent.light", "accent.dark"),
+                cursor: "pointer",
+              }}
+            />
+          </Box>
+
+          <Spacer />
+          <Box as="a" href="https://www.behance.net/akinloluwa_adedijo">
+            <Icon
+              as={FaBehance}
+              boxSize={{ base: 50, md: 75 }}
+              color={useColorModeValue("primary.light", "primary.dark")}
+              _hover={{
+                color: useColorModeValue("accent.light", "accent.dark"),
+                cursor: "pointer",
+              }}
+            />
+          </Box>
+        </Flex>
+      </Box>
+    </Center>
   );
 };
 
