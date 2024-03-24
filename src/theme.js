8f6e687721727f0ba9ee07ff6a83375a6acf746f
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2024-03-23 22:53:56 -0700

    experiment w new colors

--- src/theme.js
+++ src/theme.js
@@ -3,26 +3,49 @@ import { extendTheme } from "@chakra-ui/react";
 
 // 2. Extend the theme to include custom colrs, fonts, e.t.c
 
+// const colors = {
+//   text: {
+//     light: "#000000",
+//     dark: "#FFFFFF",
+//   },
+//   background: {
+//     light: "#E9F7F9",
+//     dark: "#181E34",
+//   },
+//   primary: {
+//     light: "#1F345F",
+//     dark: "#8FB3FF",
+//   },
+//   secondary: {
+//     light: "#EBF1FF",
+//     dark: "#001952",
+//   },
+//   accent: {
+//     light: "#D41D6C",
+//     dark: "#F199BF",
+//   },
+// };
+
 const colors = {
   text: {
-    light: "#000000",
-    dark: "#FFFFFF",
+    light: "#141110",
+    dark: "#efeceb",
   },
   background: {
-    light: "#E9F7F9",
-    dark: "#181E34",
+    light: "#ede9e8",
+    dark: "#171312",
   },
   primary: {
-    light: "#1F345F",
-    dark: "#8FB3FF",
+    light: "#51443e",
+    dark: "#c1b4ae",
   },
   secondary: {
     light: "#EBF1FF",
     dark: "#001952",
   },
   accent: {
-    light: "#D41D6C",
-    dark: "#F199BF",
+    light: "#5b765e",
+    dark: "#89a48c",
   },
 };
 
