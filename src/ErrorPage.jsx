98fafc9bde243f546534187001e35c7dd7b4336d
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2024-03-25 22:20:11 -0700

    styled error page

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,19 +1,44 @@
 import { useRouteError } from "react-router-dom";
-import { Box, Heading, Center, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Heading,
+  VStack,
+  Center,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 
 export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <Box h={"100%"} w={"100%"}>
-      <Center>
-        <Heading>Oops</Heading>
-        <Text>Seems you've navigted to a page that does not exist</Text>
+    <Center h={"100vh"}>
+      <VStack>
+        <Heading fontSize={64}>Oops</Heading>
+        <Text fontSize={28}>
+          Seems you've navigted to a page that does not exist
+        </Text>
+        <Box
+          as="a"
+          px={2}
+          py={1}
+          rounded={"md"}
+          href={"/"}
+          color={useColorModeValue("text.light", "text.dark")}
+          _hover={{
+            textDecoration: "none",
+            bg: useColorModeValue("primary.light", "primary.dark"),
+            color: useColorModeValue("text.dark", "text.light"),
+          }}
+          fontSize={"lg"}
+        >
+          Head Back to Akinloluwa Adedijo's Website
+        </Box>
         <p>
           <i>{error.statustext || error.message}</i>
         </p>
-      </Center>
-    </Box>
+      </VStack>
+    </Center>
   );
 }
