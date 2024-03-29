4bbb9b02001693276f45984c67f0432bda0036c9
Akinloluwa-Adedijo <adedijoakinloluwa@gmail.com>
2024-03-29 12:12:18 -0700

    removed previous colors

--- src/theme.js
+++ src/theme.js
@@ -3,29 +3,6 @@ import { extendTheme } from "@chakra-ui/react";
 
 // 2. Extend the theme to include custom colrs, fonts, e.t.c
 
-// const colors = {
-//   text: {
-//     light: "#000000",
-//     dark: "#FFFFFF",
-//   },
-//   background: {
-//     light: "#E9F7F9",
-//     dark: "#181E34",
-//   },
-//   primary: {
-//     light: "#1F345F",
-//     dark: "#8FB3FF",
-//   },
-//   secondary: {
-//     light: "#EBF1FF",
-//     dark: "#001952",
-//   },
-//   accent: {
-//     light: "#D41D6C",
-//     dark: "#F199BF",
-//   },
-// };
-
 const colors = {
   text: {
     light: "#141110",
